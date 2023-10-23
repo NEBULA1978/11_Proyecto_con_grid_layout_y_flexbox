bea4c5be2b814d738c63e19a818aa62dfbeb8a1b
NEBULA1978 <rpascualey@gmail.com>
2023-10-23 23:11:57 +0200

    Ejemplos añadir fotos con javascript

--- proyecto/js/main.js
+++ proyecto/js/main.js
@@ -1,4 +1,6 @@
+// Seleccoionamos por id caja imagenes
 var photoContainer = document.getElementById("photo-container");
+
 
 content = '<div class="column">';
 for(let i=0; i< 20; i++){
@@ -13,8 +15,25 @@ for(let i=0; i< 20; i++){
 
 content += "</div>";
 
-photoContainer.innerHTML = content;
+// Para ver foto 1
+// photoContainer.innerHTML = getPhotoElement(1);
+// Para ver foto 2
+// photoContainer.innerHTML = getPhotoElement(2);
 
+// Para añdir foto 1
+// photoContainer.innerHTML += getPhotoElement(1);
+// Para añdir foto 2
+// photoContainer.innerHTML += getPhotoElement(2);
+// Veria dos fotos
+
+
+// Añadir 20 fotos con un bucle for con inneHTML
+// for(let i=0; i< 20; i++){
+//     photoContainer.innerHTML += getPhotoElement(i+1);
+// }
+
+photoContainer.innerHTML = content;
+// Crear nombre fotos por id
 function getPhotoElement(photo_id){
     return `<a href="#" class="photo">
         <img loading="lazy" alt="Photo ${photo_id}" src="img/photo_${photo_id}.jpg" />
